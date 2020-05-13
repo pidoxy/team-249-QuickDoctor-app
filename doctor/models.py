aa987884c03411efc600d763c8fd4ff1b98cfe17
Ego Ugwu <egougwu11@yahoo.com>
2020-05-13 23:53:26 +0100

    First commit

---
+++ doctor/models.py
@@ -0,0 +1,26 @@
+from django.db import models  
+
+class Doctor(models.Model):    
+    name = models.CharField(max_length=100)
+    field = models.CharField(max_length=100)  
+    email = models.EmailField()  
+    contact = models.CharField(max_length=20 )  
+    address = models.CharField(max_length=100)
+    class Meta:  
+        db_table = "doctor"  
+
+    
+    def __str__(self):
+        return self.name
+
+    
+        """
+
+class Patient(models.Model):    
+    name = models.CharField(max_length=100) 
+    contact = models.IntegerField(max_length=15)  
+    address = models.TextField(max_length=100)
+    doctors = models.ManyToManyField(Doctor)
+    class Meta:  
+        db_table = "doctor"  
+        """
