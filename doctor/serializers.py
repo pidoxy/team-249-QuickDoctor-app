afa1fd7df2f3948406e2a93d2f14084bd7ee83a4
Ego Ugwu <egougwu11@yahoo.com>
2020-05-14 13:46:27 +0100

    First commit

--- doctor/serializers.py
+++ doctor/serializers.py
@@ -1,25 +1,11 @@
 from rest_framework import serializers
-#*from rest_framework.request import Request
-#*from rest_framework.test import APIRequestFactory
+
 from .models import Doctor
-#*from .serializers import DoctorSerializer
-"""
-factory = APIRequestFactory()
-request = factory.get('/')
 
 
-serializer_context = {
-    'request': Request(request),
-}
-
-d = Doctor.objects.first()
-s = DoctorSerializer(instance=p, context=serializer_context)
-
-print (s.data)
-"""
 
 class DoctorSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Doctor
-        fields = ('__all__') #* ('name', 'field', 'email', 'contact', 'address'  )
+        fields = ('__all__') 
         
